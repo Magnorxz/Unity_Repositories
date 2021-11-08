9aa9f07726ec9521f5fd9c8f259d79e98222489a
Magnorxz <angeljose188@gmail.com>
2021-11-08 12:37:17 +0100

    Interact with combat added

---
+++ Project RPG/Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour
+    {
+        public void Attack(CombatTarget target)
+        {
+            print("Take that you short, squat peasant");
+        }
+    }
+}
+
